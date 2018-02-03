f78b5fa95c689c279245494f32ab3e002c954d14
gnudeb <gnudeb0@gmail.com>
2018-02-03 18:15:20 +0200

    Add /status command

--- bot/handlers.py
+++ bot/handlers.py
@@ -9,3 +9,20 @@ def unrecognized(request):
 
 def me(request):
     return Response("{0}".format(request.user.id))
+
+def status(request):
+    location = request.user.location or "Not set"
+    time = request.user.notification_time or "Not set"
+    active = request.user.active
+
+    response_text = (
+        "Location: {location}\n"
+        "Notification time: {time}\n"
+        "Bot active: {active}"
+    ).format(
+        location=location,
+        time=time,
+        active=active,
+    )
+
+    return Response(response_text)
--- bot/paths.py
+++ bot/paths.py
@@ -2,6 +2,7 @@ from . import handlers
 from .misc import Path
 
 paths = [
+    Path(r'^/status$', handlers.status),
     Path(r'^me$', handlers.me),
     Path(r'^ping$', handlers.ping),
     Path(r'.*', handlers.unrecognized),
